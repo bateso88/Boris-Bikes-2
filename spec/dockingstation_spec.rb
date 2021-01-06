d0fe23b3c9f2d672dfeec35e2e9ffc46f7b3726e
Richard Turney <r.turney93@gmail.com>
2021-01-06 15:12:54 +0000

    Challenge 10 complete

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,6 +1,17 @@
 require "DockingStation"
 describe DockingStation do
 
-  it {is_expected.to respond_to :release_bike}
-  
+  context "release bike function" do
+
+
+    it {is_expected.to respond_to :release_bike}
+
+    it "should return a bike" do
+        expect(DockingStation.new.release_bike).to be_a Bike
+    end
+
+    it "should be a working bike" do
+        expect(Bike.new.working?).to eq true
+    end
+  end
 end
